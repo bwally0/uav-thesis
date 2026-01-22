da4d58cd3f2156ac802026f71787a8bd771d5c88
Brendan Waldrop <bwaldropw@aol.com>
2026-01-22 09:22:00 -0700

    feat: control node pkg init

---
+++ drone_ws/src/uav_control/setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'uav_control'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'),
+            glob(os.path.join('launch', '*.launch.py'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='bwally',
+    maintainer_email='bwaldropw@aol.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'offboard_node = uav_control.src.offboard_node:main',
+        ],
+    },
+)
---
+++ drone_ws/src/uav_control/src/offboard_node.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# 2Hz handshake 
+import rclpy
+from rclpy.node import Node
+
+class OffbaordNode(Node):
+    def __init__(self):
+        super().__init__('offboard_node')
+        self.get_logger().info('Offboard node has been started.')
+
+        
+def main(args=None):
+    rclpy.init(args=args)
+
+    offboard_node = OffbaordNode()
+
+    rclpy.spin(offboard_node)
+
+    offboard_node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
